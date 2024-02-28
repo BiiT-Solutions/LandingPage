2d26da28c7a6c758bd5bccd99967bd0c0d4d4d71
Jorge Hortelano <jhortelano@biit-solutions.com>
2024-02-28 09:34:05 +0100

    Corrected URLs

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -1,9 +1,10 @@
 export class Environment {
   public static readonly ROOT_URL: string = 'DOCKER:PROTOCOL://DOCKER:MACHINE_DOMAIN';
-  public static readonly FACTS_DASHBOARD_URL: string = 'DOCKER:FACTS_DASHBOARD_URL';
-  public static readonly USER_MANAGER_URL: string = 'DOCKER:USER_MANAGER_URL';
-  public static readonly CADT_URL: string = 'DOCKER:CADT_URL';
-  public static readonly APPOINTMENT_CENTER_URL: string = 'DOCKER:APPOINTMENT_CENTER_URL';
-  public static readonly XFORMS_URL: string = 'DOCKER:XFORMS_URL';
-  public static readonly SURVEYS_URL: string = 'DOCKER:SURVEYS_URL';
+  public static readonly FACTS_DASHBOARD_SERVER: string = 'DOCKER:FACTS_DASHBOARD_URL';
+  public static readonly USER_MANAGER_SERVER: string = 'DOCKER:USER_MANAGER_URL';
+  public static readonly CADT_SERVER: string = 'DOCKER:CADT_URL';
+  public static readonly APPOINTMENT_CENTER_SERVER: string = 'DOCKER:APPOINTMENT_CENTER_URL';
+  public static readonly XFORMS_SERVER: string = 'DOCKER:XFORMS_URL';
+  public static readonly SURVEYS_CREDIBILITY_SERVER: string = 'DOCKER:SURVEYS_CREDIBILITY_URL';
+  public static readonly SURVEYS_NCA_SERVER: string = 'DOCKER:SURVEYS_NCA_URL';
 }
