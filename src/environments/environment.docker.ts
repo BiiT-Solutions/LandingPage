708f9144fd327de69cfba3fe2a7119de42a4e0d2
Jorge Hortelano <jhortelano@biit-solutions.com>
2024-05-22 10:42:24 +0200

    Added boarding pass

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -4,6 +4,7 @@ export class Environment {
   public static readonly USER_MANAGER_SERVER: string = 'DOCKER:USER_MANAGER_URL';
   public static readonly CADT_SERVER: string = 'DOCKER:CADT_URL';
   public static readonly APPOINTMENT_CENTER_SERVER: string = 'DOCKER:APPOINTMENT_CENTER_URL';
+  public static readonly BOARDING_PASS_SERVER: string = 'DOCKER:BOARDING_PASS_URL';
   public static readonly XFORMS_SERVER: string = 'DOCKER:XFORMS_URL';
   public static readonly SURVEYS_CREDIBILITY_SERVER: string = 'DOCKER:SURVEYS_CREDIBILITY_URL';
   public static readonly SURVEYS_NCA_SERVER: string = 'DOCKER:SURVEYS_NCA_URL';
